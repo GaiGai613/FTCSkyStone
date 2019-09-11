0f6cf55c4419a593eb3950668f4ca6201e6075f2
GaiGai <henhaodegaigai@gmail.com>
2019-09-10 22:51:02 -0400

    Added more features to Vector3 and fixed Main

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import java.util.List;
+
+import FTCEngine.Core.Behavior;
+import FTCEngine.Core.Main;
+
+public class TestOp extends Main
+{
+	@Override
+	public void addBehaviors(List<Behavior> behaviorList)
+	{
+
+	}
+}
