1889b542fcd3d5e177bdee47b9332d1e9072bdeb
GaiGai <henhaodegaigai@gmail.com>
2019-10-03 17:17:04 -0400

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBehavior.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.io.Console;
+
+import FTCEngine.Core.Behavior;
+import FTCEngine.Core.Main;
+
+public class TestBehavior extends Behavior
+{
+	public TestBehavior(Main opMode)
+	{
+		super(opMode);
+	}
+
+	@Override
+	public void awake(HardwareMap hardwareMap)
+	{
+		super.awake(hardwareMap);
+
+		telemetry.addData("AWAKE!", "YAYAY");
+		System.out.println("AWAKE!");
+	}
+
+	@Override
+	public void start()
+	{
+		super.start();
+
+		telemetry.addData("START!", "YEAH");
+		telemetry.addData("oh", opMode.getBehavior(OhDrive.class));
+
+		System.out.println("START!");
+		System.out.println(opMode.getBehavior(OhDrive.class));
+	}
+
+	@Override
+	public void update()
+	{
+		super.update();
+
+		telemetry.addData("DELTA TIME!", time.getDeltaTime());
+		telemetry.addData("TOTAL TIME!", time.getTime());
+	}
+
+	@Override
+	public void stop()
+	{
+		super.stop();
+
+		telemetry.addData("STOP!", "NOoooo");
+	}
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp.java
@@ -1,15 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import java.util.List;
 
 import FTCEngine.Core.Behavior;
 import FTCEngine.Core.Main;
 
-public class TestOp extends Main
+@TeleOp(name = "SOMETHING THAT SHOULD")
+public class TestOp extends LinearOpMode
 {
+//	@Override
+//	public void addBehaviors(List<Behavior> behaviorList)
+//	{
+////		behaviorList.add(new OhDrive(this));
+////		behaviorList.add(new TestBehavior(this));
+//	}
+
+
 	@Override
-	public void addBehaviors(List<Behavior> behaviorList)
+	public void runOpMode() throws InterruptedException
 	{
-		behaviorList.add(new OhDrive(this));
+		waitForStart();
+
+		while (opModeIsActive())
+		{
+			telemetry.addData("HA", "LOL");
+			telemetry.update();
+		}
 	}
 }
